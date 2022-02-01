dc2e1973fe373a07000c0b80d914a2e80a9a877a
Nickolas Fuentes <nickfuentes24@gmail.com>
2022-01-31 19:57:05 -0600

    Created some page and nested pages to see how Gatsby handles urls.

---
+++ src/pages/about.js
@@ -0,0 +1,7 @@
+import React from "react"
+
+const about = () => {
+  return <div>Hello from about page!</div>
+}
+
+export default about
---
+++ src/pages/company/index.js
@@ -0,0 +1,7 @@
+import React from "react"
+
+const index = () => {
+  return <div>Hello from Company page.</div>
+}
+
+export default index
