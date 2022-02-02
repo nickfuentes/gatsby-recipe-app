e446788bdb562191c68047b3962dac0d4b83da58
Nickolas Fuentes <nickfuentes24@gmail.com>
2022-02-02 17:45:43 -0600

    Added css modules for styling. And added them to the index and about pages.

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,12 +1,18 @@
 import React from "react"
 
 import Layout from "../components/Layout"
+import { page, text } from "../examples/about.module.css"
 
 const about = () => {
   return (
     <Layout>
-      <div className="about-heading">
-        <h1>About page</h1>
+      <div className={page}>
+        <h1>about page</h1>
+        <h1>hello world</h1>
+        <p className={text}>
+          Lorem, ipsum dolor sit amet consectetur adispscing elit. Labroism
+          beathe natus aperiam, voluptas discntinctuio ut odio.
+        </p>
       </div>
     </Layout>
   )
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,14 +2,19 @@ import React from "react"
 import { Link } from "gatsby"
 
 import Layout from "../components/Layout"
+import { page, text } from "../examples/home.module.css"
 
 export default function Home() {
   return (
     <Layout>
-      <div className="home-heading">
+      <div className={page}>
         <h1>Home Page</h1>
         <h1>Hello World</h1>
         <h1>Hello World</h1>
+        <p>
+          This yellow brown quick fox jumped over something but I can not
+          remember what it was so I am just typing things for testing.
+        </p>
       </div>
     </Layout>
   )
