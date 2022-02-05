5f73cc99364ed57cbd61aee33ba692b7bab8cfaa
Nickolas Fuentes <nickfuentes24@gmail.com>
2022-02-04 21:44:35 -0600

    Added links and copyright date to footer.

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,9 +2,13 @@ import React from "react"
 
 const Footer = () => {
   return (
-    <div>
-      <footer>Hello from footer</footer>
-    </div>
+    <footer className="page-footer">
+      <p>
+        &copy; {new Date().getFullYear()} <span>SimplyRecipe</span>. Built
+        with&nbsp;
+        <a href="https://www.gatsbyjs.com">Gatsby</a>
+      </p>
+    </footer>
   )
 }
 
