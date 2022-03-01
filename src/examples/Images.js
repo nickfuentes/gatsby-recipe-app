9b8627335f2583beb6e07d4f0f4fab2402363e47
Nickolas Fuentes <nickfuentes24@gmail.com>
2022-03-01 12:07:03 -0600

    Added Static Image Plugin to help with image file size on page load.

--- src/examples/Images.js
+++ src/examples/Images.js
@@ -1,11 +1,17 @@
 import React from "react"
 import styled from "styled-components"
 import big from "../assets/images/big.jpg"
+import { StaticImage } from "gatsby-plugin-image"
 
 const Images = () => {
   return (
     <Wrapper>
-      <img src={big} alt="food" />
+      <StaticImage src="../assets/images/big.jpg" alt="food" />
+      <StaticImage
+        src="https://res.cloudinary.com/diqqf3eq2/image/upload/v1613583026/course%20slides/js-2_mo9uqy.png"
+        alt="Code"
+      />
+
       <h2>Gatsby Image</h2>
     </Wrapper>
   )
